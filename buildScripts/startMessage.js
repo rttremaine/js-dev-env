dcae2107a682ccf17df204c691b74f994c8199e4
Richard Tanner-Tremaine <rttremaine@dthomas.co.uk>
2017-06-06 10:57:55 +0100

    work in progess

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,6 @@
-var chalk = require("chalk");
+import chalk from 'chalk';
+
+// writing to console is OK in build scripts
+/* eslint-disable no-console */
 
 console.log(chalk.green("Starting app in dev mode..."));
